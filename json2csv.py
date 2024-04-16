f5d8acdb6d78a49d545993e5467db6351288039e
Nix3732 <116956559+Nix3732@users.noreply.github.com>
2024-04-16 18:18:00 +0200

    Update json2csv.py

--- json2csv.py
+++ json2csv.py
@@ -3,9 +3,9 @@ import pandas as pd
 from pandas import json_normalize
 import json
 
-with open('1.json') as json_file:
+with open('4.json') as json_file:
     data = json.load(json_file)
 flat_data = json_normalize(data, 'Employees')
 
-flat_data.to_csv('1.csv', index=False)
+flat_data.to_csv('4.csv', index=False)
 
